0e2de252da0799ea2336e8bff516c597fcdfd69d
Liviru <132646852+LiviruRMIT@users.noreply.github.com>
2024-09-21 15:22:38 +1000

    Implement basic ui

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,24 +1,24 @@
-import Image from "next/image";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableFooter,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+import "../globals.css";
 
 export default function Home() {
   return (
-    <>
-      <h1 className="flex justify-center">Semi Compsci</h1>
-      <div className="flex">
-        <div>#1</div>
-        <div>Liov</div>
-        <div>5000</div>
+    <div className="flex items-center min-h-screen flex-col text-xl">
+      <h1 className="text-4xl text-center mb-6">Semester 1 Computer Science</h1>
+      <div className="flex w-96 justify-center content-center">
+        <div className="flex-none w-14 text-center">1</div>
+        <div className="flex-1 w-64 text-center">Liv</div>
+        <div className="flex-none w-14 text-center">8000</div>
       </div>
-    </>
+      <div className="flex w-96 justify-center content-center">
+        <div className="flex-none w-14 text-center">2</div>
+        <div className="flex-1 w-64 text-center">Liv</div>
+        <div className="flex-none w-14 text-center">8000</div>
+      </div>
+      <div className="flex w-96 justify-center content-center">
+        <div className="flex-none w-14 text-center">3</div>
+        <div className="flex-1 w-64 text-center">Liv</div>
+        <div className="flex-none w-14 text-center">8000</div>
+      </div>
+    </div>
   );
 }
