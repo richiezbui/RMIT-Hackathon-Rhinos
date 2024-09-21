5c8481c3869bd8a7ae881379ba3ebf906c65300b
ritaCordeiro <s3947881@student.rmit.edu.au>
2024-09-21 20:22:25 +1000

    quizpage sketch

--- src/app/quizPage/page.tsx
+++ src/app/quizPage/page.tsx
@@ -3,8 +3,16 @@ import prisma from '../../lib/prisma';
 import { MainNav } from "@/components/ui/main-nav";
 import { UserNav } from "@/components/ui/user-nav";
 
-export default function Quizzes() {
+export default function QuizPage() {
+    const questions = prisma.question.findMany();
+    
     return(
-        <div>Hellow</div>
+
+        //Question
+        
+        //List of options and answers
+
+
+        // Next Question
     )
 }
