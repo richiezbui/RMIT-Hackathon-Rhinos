6360ced9108e576779f66386e56fa68118aa6ce3
ritaCordeiro <s3947881@student.rmit.edu.au>
2024-09-21 15:11:55 +1000

    Added cardcomponent, created Question model

---
+++ prisma/migrations/20240921050657_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Question" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "weekLabel" INTEGER NOT NULL,
+    "question" TEXT NOT NULL,
+    "answer" TEXT NOT NULL
+);
