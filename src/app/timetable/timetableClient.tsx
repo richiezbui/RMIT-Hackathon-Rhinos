8132b876495ef8aeaa0058c69f220acca0fa7288
ChristopherJordanLamb <s3945643@student.rmit.edu.au>
2024-09-21 17:01:34 +1000

    timetablepage

---
+++ src/app/timetable/timetableClient.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useState } from 'react';
+import { Calendar, momentLocalizer, Views } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment';
+
+import { Class } from '@prisma/client';
+
+type ClassWithLocation = Class & {
+  locations: { address: string }[];
+};
+
+interface TimetableClientProps {
+  classes: ClassWithLocation[];
+}
+
+const localizer = momentLocalizer(moment);
+
+export default function TimetableClient({ classes }: TimetableClientProps) {
+
+  const [view, setView] = useState<Views>('week');
+
+
+  const events = classes.map((classItem) => ({
+    title: `${classItem.name} (${classItem.room})`,
+    start: new Date(classItem.startTime),
+    end: new Date(classItem.endTime),
+    location: classItem.locations.map((location) => location.address).join(', '),
+  }));
+
+  return (
+    <div style={{ height: '700px' }}>
+      <h1>Class Timetable</h1>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        view={view}     
+        onView={(newView) => setView(newView)}  
+        toolbar={true}    
+        views={['month', 'week', 'day', 'agenda']}
+      />
+    </div>
+  );
+}
